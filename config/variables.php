17627c04cb5c127a0d56ce9cfa723029c032685b
ikenna <ogbuanuikenna66@gmail.com>
2024-04-08 08:46:07 +0100

    INITIAL COMMIT

---
+++ config/variables.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "appStatus" => ["pending" => "PENDING", "active" => "ACTIVE"],
+    "approvalStatus" => ["pending" => "PENDING", "approved" => "APPROVED", "declined" => "DECLINED"],
+    "dateFormat" => "M dS, Y",
+    "bool" => ["true" => "TRUE", "false" => "FALSE"],
+
+];
