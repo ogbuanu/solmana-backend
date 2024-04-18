7145f453a66419e24054e586c51d43ea22f14b80
Ogbechi Arinze <88964572+arinze-dev@users.noreply.github.com>
2024-04-18 20:59:06 +0100

    add block pass view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,11 +79,9 @@ class UserController extends Controller
         $response = $Response::get();
           $data = (object)$request->all();
 
-
           try {
-
-        $secretKey = config('services.blockpass.secret_key');
-        $webhookData = $request->getContent();
+             $secretKey = config('services.blockpass.secret_key');
+             $webhookData = $request->getContent();
 
          Log::info(json_encode($webhookData));
         
@@ -99,7 +97,8 @@ class UserController extends Controller
             Log::info('Blockpass webhook verified successfully.');
 
                $now =Carbon::now();
-            if ($data->status === "approved") {
+
+              if ($data->event === "user.approved") {
                User::where(['id'=>$data->refId, 'email'=>$data->email])->update(["kyc_verified"=>"TRUE",'kyc_verified_at'=> $now,"kyc_status"=>'APPROVED']);
                  $kycEarning =  ActionPoint::where(["user_id" => $data->refId])->first();
 
@@ -108,27 +107,49 @@ class UserController extends Controller
                      $kycEarning->balance = $kycEarning->balance +10;
                      $kycEarning->save();
                  }
-            }
-         Log::info('successfully.');
+              }
+            Log::info('successfully.');
            
-            if ($data->status === "inreview" || $data->status === "waiting") {
+            if ($data->status === "user.inreview" || $data->status === "user.waiting") {
                User::where(['id'=>$data->refId, 'email'=>$data->email])->update(["kyc_verified"=>"FALSE",'kyc_verified_at'=> $now,"kyc_status"=>'PENDING']);   
             }
 
-            if ($data->status === "rejected") {
+            if ($data->status === "review.rejected") {
                User::where(['id'=>$data->refId, 'email'=>$data->email])->update(["kyc_verified"=>"FALSE",'kyc_verified_at'=> $now,"kyc_status"=>'REJECTED']);   
             }
-          Log::info('Blockpass webhook verified successfully.');
-            return response()->json(['message' => 'Webhook verified successfully'], 200);
-        } else {
+
+
+
+
+
+            // if ($data->status === "approved") {
+            //    User::where(['id'=>$data->refId, 'email'=>$data->email])->update(["kyc_verified"=>"TRUE",'kyc_verified_at'=> $now,"kyc_status"=>'APPROVED']);
+            //      $kycEarning =  ActionPoint::where(["user_id" => $data->refId])->first();
+
+            //      if ($kycEarning->last_kyc_earning === null) {
+            //          $kycEarning->last_kyc_earning = $now;
+            //          $kycEarning->balance = $kycEarning->balance +10;
+            //          $kycEarning->save();
+            //      }
+            // }
+            // Log::info('successfully.');
+           
+            // if ($data->status === "inreview" || $data->status === "waiting") {
+            //    User::where(['id'=>$data->refId, 'email'=>$data->email])->update(["kyc_verified"=>"FALSE",'kyc_verified_at'=> $now,"kyc_status"=>'PENDING']);   
+            // }
+
+            // if ($data->status === "rejected") {
+            //    User::where(['id'=>$data->refId, 'email'=>$data->email])->update(["kyc_verified"=>"FALSE",'kyc_verified_at'=> $now,"kyc_status"=>'REJECTED']);   
+            // }
+
+              Log::info('Blockpass webhook verified successfully.');
+              return response()->json(['message' => 'Webhook verified successfully'], 200);
+           } else {
             // Signature verification failed, log an error
-            Log::error('Blockpass webhook verification failed.');
+              Log::error('Blockpass webhook verification failed.');
 
             return response()->json(['error' => 'Webhook verification failed'], 403);
-        }
-
- 
-   
+           }
           } catch (\Throwable $th) {
              $response = $Response::set(["message" => "{$th->getMessage()}"], false);
           }catch (\Exception $e) {
@@ -141,11 +162,12 @@ class UserController extends Controller
         $Response = new Response();
         $response = $Response::get();
         $data = (object)$request->all();
+             $key =  env('TWITTER_CONSUMER_KEY');
 
-            $twitterConsumerKey = "WZbMssxskLYLqkq7GIl8Jhguq";
-            $twitterConsumerSecret = "Wd4dXLQySBXZdYrLCZuaRBfdkqy0wRBPLgTerJ8geQrvaYDJtI";
-            $twitterAccessToken = "1076644233480683526-flIHl6J8M1MGbbGqR1J8hNvXfr84f8";
-            $twitterAccessTokenSecret = "GbamxnoicZlcX0GekkUwWiOp5rNxQkT7trTVDGcf53H76";
+            $consumer_key = "WZbMssxskLYLqkq7GIl8Jhguq";
+            $consumer_secret = "Wd4dXLQySBXZdYrLCZuaRBfdkqy0wRBPLgTerJ8geQrvaYDJtI";
+            $access_token = "1076644233480683526-flIHl6J8M1MGbbGqR1J8hNvXfr84f8";
+            $access_token_secret = "GbamxnoicZlcX0GekkUwWiOp5rNxQkT7trTVDGcf53H76";
 
             $tweetBearer="AAAAAAAAAAAAAAAAAAAAAB5WtQEAAAAAXK%2BqVYFW3guPKwe3jQX8QAc82kQ%3D60eXjtJtDebLVoEl70KfhERn0ijv48NFX9e26GTVi4Gv8aX8yk";
            
@@ -157,6 +179,13 @@ class UserController extends Controller
 
                $tweetId ="1780107798908440863";
 
+
+
+        
+
+
+               print_r($key);
+
             //   $connection = new TwitterOAuth(
             //     $twitterConsumerKey,
             //     $twitterConsumerSecret,
@@ -164,6 +193,22 @@ class UserController extends Controller
             //     $twitterAccessTokenSecret
             // );
 
+
+                $connection = new TwitterOAuth($consumer_key, $consumer_secret, $access_token, $access_token_secret);  
+
+                  $connection->setApiVersion('2');
+
+  
+            $tweets = $connection->get("status/show", ['id' => $tweetId,]);
+
+
+     print_r($tweets);
+         dd($tweets);
+     
+                
+
+
+    // https://api.twitter.com/2/tweets/$tweetId
             // $response = $connection->get("statuses/show", [
             //     'id' => $tweetId,
             // ]);
@@ -179,14 +224,11 @@ class UserController extends Controller
 
             // return response()->json($response);
 
-
-  
-          
-
-              //  $response = Http::withToken($tweetBearer)
+  // 
+              //  $respon = Http::withheaders(["authorization"=> "Bearer" . $tweetBearer])
               //   ->get("https://api.twitter.com/2/tweets/$tweetId");
 
-            //              dd($response);
+              //            dd($respon);
 
             // if ($response->successful()) {
             //     $data = $response->json();
@@ -204,13 +246,13 @@ class UserController extends Controller
         // Validate credentials (optional)
    
 
-        $encodedCredentials = base64_encode("$twitterConsumerKey:$twitterConsumerSecret");
+        // $encodedCredentials = base64_encode("$twitterConsumerKey:$twitterConsumerSecret");
 
    
-            $respse = Http::withBasicAuth('', $encodedCredentials)
-                ->get("https://api.twitter.com/2/tweets/$tweetId");
+        //     $respse = Http::withBasicAuth('', $encodedCredentials)
+        //         ->get("https://api.twitter.com/2/tweets/$tweetId");
 
-          dd($respse);
+        //   dd($respse);
 
         
 
@@ -229,6 +271,12 @@ class UserController extends Controller
           }
         return response()->json($response,  $response->code);
     }
+
+
+    public function kyc(Request $request){
+       view("kyc");
+    }
+
 
 }
 
