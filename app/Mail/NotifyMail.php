d60e33237145081a92d0159cfe04d2d10b05aec8
Ogbechi Arinze <88964572+arinze-dev@users.noreply.github.com>
2024-05-01 03:03:42 +0100

    fix on email

---
+++ app/Mail/NotifyMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NotifyMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data =[];
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(array $data)
+    {
+
+        $this->data = $data;
+    }
+
+        /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        //appName,domain,username,activateLink,appMail
+        return $this->view('email.notify')->subject($this->data['subject']);
+    }
+
+}
