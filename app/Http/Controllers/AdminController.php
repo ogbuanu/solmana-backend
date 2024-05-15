cea85e4d2ea0afaf804457eb83f638d44eb79e91
ikenna <ogbuanuikenna66@gmail.com>
2024-05-15 09:43:48 +0100

    Updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -99,7 +99,7 @@ class AdminController extends Controller
         $Response = new Response();
         $response = $Response::get();
         $data = (object) $request->all();
-        $approvalStatus = config('data.approval');
+        $approvalStatus = (object) config('data.approval');
         $user = auth()->user();
         try {
             if ($user->role != "ADMIN") {
